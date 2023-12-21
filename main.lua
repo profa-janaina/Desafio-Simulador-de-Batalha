e11ec9f53cf1aea9ae0b1bd78700800be7227a7a
Janaina <profa.janaina01@gmail.com>
2023-12-20 22:42:53 -0300

    Add header

---
+++ main.lua
@@ -0,0 +1,18 @@
+-- Habilitar o UTF8
+os.execute("chcp 65001")
+
+--Header
+print([[
+
+*************************************************
+              /| ________________
+        O|===|* >________________>
+              \|
+
+             - SIMULADOR DE BATALHA -
+
+*************************************************
+                É hora da batalha!
+    Empunhe a sua espada e prepare-se para a luta.
+
+]])
