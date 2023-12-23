de05eb472aa666e925024014e66e08bb9a9825f5
Janaina <profa.janaina01@gmail.com>
2023-12-23 00:19:41 -0300

    Add player actions

---
+++ characteres/player/actions.lua
@@ -0,0 +1,77 @@
+local actions = {}
+
+actions.list = {}
+
+---
+---Cria uma lista de ações armazenadas internamente
+---
+function actions.build()
+    -- Resetar lista
+    actions.list = {}
+
+    --Atacar com espada
+    local swordAttack ={
+        description = "Atacar com a espada.",
+        requirement = nil,
+        execute = function (playerData, creatureData)
+            --1. Definir chance de sucesso
+            local sucessChance = creatureData.speed == 0 and 1 or playerData.speed / creatureData.speed
+            local sucess = math.random() <= sucessChance
+            
+            --2. Apresentar resultado como print
+            if sucess then
+                --3. Calcular dano
+                local rawDamage = playerData.attack - math.random() * creatureData.defense
+                local damage = math.max(1, math.ceil(rawDamage))
+
+                print(string.format("Você atacou a criatura causando %d pontos de dano", damage))
+                
+                --4. Aplicar o dano
+                creatureData.health = creatureData.health - damage
+            else
+                print("O seu ataque falhou, treine mais e tente novamente")  
+            end
+        end
+    }
+
+    -- Usar poção de regeneração
+    local regenPotion ={
+        description = "Atacar com a espada.",
+        requirement = function (playerData, creatureData)
+            return playerData.potions >= 1
+        end,
+        execute = function (playerData, creatureData)
+            --Tirar poção do inventário
+            playerData.potions =  playerData.potions -1
+
+            -- Recuperar vida
+            local regenPoints = 5
+            playerData.health = math.min(playerData.maxHealth, playerData.health + regenPoints)
+            print("Você usou uma poção e recuperou pontos de vida")
+        end
+    }
+
+    --Adicionar ação a lista
+    actions.list[#actions.list+1] = swordAttack
+    -- actions.list[#actions.list+1] = regenPotion
+end
+
+---
+---Retorna uma lista de ações válidas
+---@param playerData table
+---@param creatureData table
+---@return table
+---
+function actions.getValidActions(playerData, creatureData)
+    local validaActions = {}
+    for _, action in pairs(actions.list) do
+       local requirement = action.requirement
+       local isValid = requirement == nil or requirement(playerData, creatureData)
+       if isValid then
+        validaActions[#validaActions+1] = action        
+       end 
+    end
+    return validaActions
+end
+
+return actions
--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 -- Dependências
-local player = require("characteres.player")
-local colossus = require('characteres.colossus')
+local player = require("characteres.player.player")
+local playerActions = require("characteres.player.actions")
+local colossus = require('characteres.colossus.colossus')
 local utils = require("utils")
 
 -- Habilitar o UTF8
@@ -13,3 +14,20 @@ local boss = colossus
 
 -- Imprimi as informações do boss
 utils.printCreature(boss)
+
+-- Loop da batalha
+while true do
+    -- Mostrar opções de ações para o jogador
+    -- Simular o turno do jogador
+    
+    -- saída: boss morre
+    if boss.health <=0 then
+        break
+    end
+    -- Simular o turno do boss
+
+    -- saída: jogador morre
+    if player.health <=0 then
+        break
+    end
+end
